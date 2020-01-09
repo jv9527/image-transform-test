908755f234f346be4d1397a83283eb0e74e0f156
Adrianus Kevin <adrianus.kusuma@tokopedia.com>
2020-01-09 13:20:38 +0700

    test vips

--- go/main.go
+++ go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/jv9527/image-transform-test/go/vips"
 )
@@ -22,27 +23,42 @@ func main() {
 	//defer imagick.Terminate()
 	/* http.HandleFunc("/upload", UploadHandler)
 	log.Fatal(grace.Serve(":9000", nil)) */
-	//vips.Resize("file2.jpg", "images/", "results/vips/", width, height, quality)
-	//graphicsmagick.Resize("file2.jpg", "images/", "results/gmagick/", width, height, quality)
+	// vips.Resize("file2.jpg", "images/", "results/vips/", width, height, quality)
+	// graphicsmagick.Resize("file2.jpg", "images/", "results/gmagick/", width, height, quality)
 
+	var wg sync.WaitGroup
+	defer wg.Wait()
 	for x := 0; x < 20; x++ {
 		go func() {
+			wg.Add(1)
 			vips.Convert("test1.png", rootDir, "results/vips/")
-
+			wg.Done()
+		}()
+		go func() {
+			wg.Add(1)
 			vips.Convert("test2.png", rootDir, "results/vips/")
-
+			wg.Done()
+		}()
+		go func() {
+			wg.Add(1)
 			vips.Convert("test3.png", rootDir, "results/vips/")
-
+			wg.Done()
+		}()
+		go func() {
+			wg.Add(1)
 			vips.Convert("test4.png", rootDir, "results/vips/")
-
+			wg.Done()
+		}()
+		go func() {
+			wg.Add(1)
 			vips.Convert("test5.png", rootDir, "results/vips/")
-
+			wg.Done()
 		}()
 
 	}
-	//vips.Convert(filename, rootDir, "results/vips/")
-	//graphicsmagick.Convert(filename, rootDir, "results/gmagick/")
-	//imgsosick.Convert(filename, rootDir, "results/imagick/")
+	// vips.Convert(filename, rootDir, "results/vips/")
+	// graphicsmagick.Convert(filename, rootDir, "results/gmagick/")
+	// imgsosick.Convert(filename, rootDir, "results/imagick/")
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
